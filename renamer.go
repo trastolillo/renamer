a4f5b53d4e8df735b9795472d01d8fc8a83ab0d1
trastolillo <trastolillo@tutanota.com>
2021-08-04 19:57:07 +0200

    funcionalidad de la opción -f

--- renamer.go
+++ renamer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
@@ -15,10 +16,11 @@ const patternRegex string = `(s\d+e\d+)|(\d+x\d+)`
 
 var regex *regexp.Regexp = regexp.MustCompile(patternRegex)
 
-var extensionesSubtitulos = [...]string{".srt", ".idx", ".sub"}
+var extensionesSubtitulos = []string{".srt", ".idx", ".sub"}
+var extensionesVideos = []string{".avi", ".mkv", ".mpg", ".mp4", ".webm", ".wmv"}
 
 var carpeta string
-var pistaArchivo string
+var cadenaArchivo string
 
 type Archivo struct {
 	nombre string
@@ -31,9 +33,9 @@ func main() {
 	listaArchivos := fileList(carpeta)
 
 	for _, subtitulo := range listaArchivos {
-		if subtitulo.esSubtitulo() {
+		if subtitulo.esTipoArchivo(extensionesSubtitulos) {
 			for _, video := range listaArchivos {
-				if !video.esSubtitulo() && reflect.DeepEqual(extraerNumeroDeCapitulo(video),
+				if video.esTipoArchivo(extensionesVideos) && reflect.DeepEqual(extraerNumeroDeCapitulo(video),
 					extraerNumeroDeCapitulo(subtitulo)) {
 					nuevoNombre := carpeta + "/" + video.nombre + subtitulo.ext
 					viejoNombre := carpeta + "/" + subtitulo.nombre + subtitulo.ext
@@ -48,7 +50,7 @@ func main() {
 func procesarFlags() {
 	// Declaraciones de Flag
 	flag.StringVar(&carpeta, "d", "./", "Renombra todos los ficheros dentro de la carpeta")
-	flag.StringVar(&pistaArchivo, "f", "", "Renombra al archivo que coincida con la cadena")
+	flag.StringVar(&cadenaArchivo, "f", "", "Renombra al archivo que coincida con la cadena")
 	// Análisis de opciones
 	flag.Parse()
 }
@@ -60,16 +62,7 @@ func fileList(carpeta string) []Archivo {
 		log.Fatal(err)
 	}
 	// Lista de archivos
-	var sliceArchivos []Archivo
-	for _, file := range files {
-		if !file.IsDir() {
-			extension := path.Ext(file.Name())
-			nombre := strings.TrimSuffix(file.Name(), extension)
-			sliceArchivos = append(sliceArchivos,
-				Archivo{nombre: nombre, ext: extension})
-		}
-	}
-	return sliceArchivos
+	return listarArchivos(files, cadenaArchivo)
 }
 
 func extraerNumeroDeCapitulo(archivo Archivo) []string {
@@ -78,11 +71,34 @@ func extraerNumeroDeCapitulo(archivo Archivo) []string {
 	return re.FindAllString(string(cadenaCompleta), -1)
 }
 
-func (this Archivo) esSubtitulo() bool {
-	for _, extension := range extensionesSubtitulos {
+func (this Archivo) esTipoArchivo(listaExtensiones []string) bool {
+	for _, extension := range listaExtensiones {
 		if this.ext == extension {
 			return true
 		}
 	}
 	return false
+}
+
+func filtrarArchivosPorNombre(listaArchivos []Archivo) []Archivo {
+	// retorno := []Archivo{}
+	return nil
+
+}
+
+func listarArchivos(files []fs.FileInfo, nombreDeArchivo string) []Archivo {
+	var sliceArchivos []Archivo
+	condicion := true
+	for _, file := range files {
+		if nombreDeArchivo != "" {
+			condicion = strings.Contains(file.Name(), nombreDeArchivo)
+		}
+		if !file.IsDir() && condicion {
+			extension := path.Ext(file.Name())
+			nombre := strings.TrimSuffix(file.Name(), extension)
+			sliceArchivos = append(sliceArchivos,
+				Archivo{nombre: nombre, ext: extension})
+		}
+	}
+	return sliceArchivos
 }
